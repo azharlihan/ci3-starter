a8707e565b6fc475f6e635d260caf38db26a8614
azharlihan <46442361+azharlihan@users.noreply.github.com>
2020-10-15 11:34:38 +0700

    autoload database library, informasi credential diambil dari .env

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => $_SERVER['DB_HOST'] ?: 'localhost',
+	'username' => $_SERVER['DB_USER'] ?: 'root',
+	'password' => $_SERVER['DB_PASS'] ?: '',
+	'database' => $_SERVER['DB_NAME'] ?: '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
